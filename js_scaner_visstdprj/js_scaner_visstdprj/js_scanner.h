0998d1e8fb47b591e7cc570bd56ebe0ee897ab06
dylanthecreeper <dylangentile@ymail.com>
2016-01-10 18:33:02 -0800

    Update js_scanner.h

--- js_scaner_visstdprj/js_scaner_visstdprj/js_scanner.h
+++ js_scaner_visstdprj/js_scaner_visstdprj/js_scanner.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <string>
+
+
+class JSChar
+{
+public:
+	char mChar; 
+	int mLineIndex;
+	int mColumnIndex;
+};
+
+
+
+class JSScanner
+{
+public:
+	JSScanner();
+	~JSScanner();
+
+
+	int open(const std::string &i_fileName);
+
+	JSChar get();
+
+private:
+
+	int mIndex;
+	int mLength;
+
+	std::string mFileName;
+	char *mBuffer;
+
+
+};
